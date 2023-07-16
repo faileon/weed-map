404816422ed82800bddaf2c3dcb01656f7a79bae
faileon <pt4cekt@gmail.com>
2023-07-16 23:06:53 +0200

    filters routing?

--- apps/weed-map/src/routes/stores/index.tsx
+++ apps/weed-map/src/routes/stores/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link, routeLoader$ } from '@builder.io/qwik-city';
+import { Link, routeLoader$, useNavigate } from '@builder.io/qwik-city';
 import { getSupabaseClient } from '@api/client';
 import { Button } from '@ui/button';
 import { Icon } from '@ui/icon';
@@ -15,10 +15,38 @@ export const head: DocumentHead = {
   ],
 };
 
+const mergeSearchQuery = (
+  params: URLSearchParams,
+  name: string,
+  value: string
+) => {
+  const newParams = new URLSearchParams(params);
+  if (newParams.has(name)) {
+    const oldParams = newParams.get(name);
+    const split = oldParams?.split(',');
+    let newValue;
+    if (split?.includes(value)) {
+      newValue = split.filter((val) => val !== value).join(',');
+    } else {
+      newValue = `${oldParams},${value}`;
+    }
+    newParams.set(name, newValue);
+  } else {
+    newParams.append(name, value);
+  }
+  return decodeURIComponent(newParams.toString());
+};
+
 export const useStores = routeLoader$(async (req) => {
+  const { query } = req;
   const client = getSupabaseClient(req);
-  const { data } = await client.from('stores').select('*');
-  return data;
+  const { data, count } = await client.from('stores').select('*');
+
+  return {
+    data,
+    count,
+    query,
+  };
 });
 
 export default component$(() => {
@@ -31,34 +59,54 @@ export default component$(() => {
     ['THC', 'spa'],
     ['CBD', 'eco'],
     ['Top rated', 'verified'],
-  ].map(([label, icon]) => ({
-    label,
-    value: label.toLowerCase().split(' ').join('-'),
-    icon,
-  }));
+    ['Debit card payment', 'credit_card'],
+    ['Cash payment', 'payments'],
+    ['Accessible', 'accessible'],
+  ]
+    .reverse()
+    .map(([label, icon]) => ({
+      label,
+      value: label.toLowerCase().split(' ').join('-'),
+      icon,
+    }));
 
   return (
     <main class="container mx-auto flex flex-col gap-4 mt-4">
       {/* ROW WITH FILTERS */}
       <div class="flex gap-8 w-full justify-center">
-        <div class="flex flex-1 gap-8 flex-row-reverse items-center">
+        <div class="flex flex-1 gap-4 flex-row-reverse items-center">
           {filters.map(({ value, label, icon }) => (
-            <Link key={value} href={`/stores?filters=${value}`}>
-              <div class="flex flex-col gap-2 items-center text-primary-600 hover:text-primary-500 w-24">
+            <Link
+              key={value}
+              href={`/stores?${mergeSearchQuery(
+                stores.value.query,
+                'filters',
+                value
+              )}`}
+            >
+              <div
+                class={[
+                  `flex flex-col gap-1 items-center  hover:text-primary-600 text-center px-4`,
+                  stores.value.query?.get('filters')?.split(',').includes(value)
+                    ? `text-primary-600 border-b-2 border-primary-600 font-bold`
+                    : `text-primary-400`,
+                ]}
+              >
                 <Icon icon={icon}></Icon>
-                <span class="font-mono">{label}</span>
+                <span class="font-mono text-sm">{label}</span>
               </div>
             </Link>
           ))}
         </div>
-        <div class="flex items-center justify-center">
+        <div class="flex items-center justify-center gap-2">
+          <Button icon="sort" variant="ghost"></Button>
           <Button icon="page_info" variant="ghost"></Button>
         </div>
       </div>
 
       {/* STORES IN GRID */}
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {stores.value?.map((store) => {
+        {stores.value?.data?.map((store) => {
           return (
             <Link
               href={`/stores/${store.slug}`}
