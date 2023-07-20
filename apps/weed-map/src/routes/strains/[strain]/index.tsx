7d7ab2896a4b9057aeb7d62cec460dc7e5e334f7
pazzicz <pazzicz@gmail.com>
2023-07-20 21:25:25 +0200

    strains - better formating + links

--- apps/weed-map/src/routes/strains/[strain]/index.tsx
+++ apps/weed-map/src/routes/strains/[strain]/index.tsx
@@ -1,8 +1,13 @@
-import {component$, useStore, useStyles$, useStylesScoped$} from '@builder.io/qwik';
-import type {DocumentHead} from '@builder.io/qwik-city';
-import {routeLoader$} from "@builder.io/qwik-city";
-import {getSupabaseClient} from "@api/client";
-import StrainStyles from './strain.css?inline'
+import {
+  component$,
+  useStore,
+  useStyles$,
+  useStylesScoped$,
+} from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link, routeLoader$ } from '@builder.io/qwik-city';
+import { getSupabaseClient } from '@api/client';
+import StrainStyles from './strain.css?inline';
 
 export const head: DocumentHead = {
   title: 'Weed Map - Strain Detail',
@@ -14,7 +19,7 @@ export const head: DocumentHead = {
   ],
 };
 
-const mockStrain =  {
+const mockStrain = {
   id: 1,
   slug: 'strain1',
   name: 'Strain name 2',
@@ -22,18 +27,19 @@ const mockStrain =  {
   rating: {
     value: 4.6,
     count: 5142,
-    likes: 82265
+    likes: 82265,
   },
   created_at: '2023-07-09T12:54:01.829421+00:00',
   updated_at: '2023-07-09T12:54:01.829421+00:00',
   genetic: 'Sativa',
   purity: { min: 90, max: 100 },
-  thc: {min: 20, max: 20 },
-  cbd: {min: 0, max: 0 },
+  thc: { min: 20, max: 20 },
+  cbd: { min: 0, max: 0 },
   price: { min: 10, max: 15, currency: 'USD' },
   parents: ['strain2'],
   aliases: ['Gorilla Glue', 'Sticky Glue'],
-  description: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+  description:
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam bibendum elit eget erat. Nullam eget nisl. Duis condimentum augue id magna semper rutrum. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Mauris metus. In enim a arcu imperdiet malesuada. Integer lacinia. Mauris elementum mauris vitae tortor. Aliquam in lorem sit amet leo accumsan lacinia. Fusce tellus. Etiam commodo dui eget wisi. Nulla pulvinar eleifend sem. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Nulla quis diam. Praesent dapibus. Quisque porta. Aliquam in lorem sit amet leo accumsan lacinia. Praesent vitae arcu tempor neque lacinia pretium.',
   feelings: [
     { name: 'relaxed', evidence: 30 },
     { name: 'sleepy', evidence: 30 },
@@ -53,120 +59,117 @@ const mockStrain =  {
     { name: 'pungent', evidence: 30 },
     { name: 'pine', evidence: 30 },
     { name: 'earthy', evidence: 30 },
-  ],    
+  ],
   terpene_dominant: 'caryophyllene',
-  terpene_other: [ 'myrcene', 'limonene' ],
+  terpene_other: ['myrcene', 'limonene'],
 };
 
 export const useStrain = routeLoader$(async (req) => {
-  const {strain} = req.params;
+  const { strain } = req.params;
   const client = getSupabaseClient(req);
-  const {data} = await client.from('strains').select('*').eq('slug', strain).single();
+  const { data } = await client
+    .from('strains')
+    .select('*')
+    .eq('slug', strain)
+    .single();
   return data;
-})
-
+});
 
 export default component$(() => {
-  const strain = useStore({...mockStrain});
+  const strain = useStore({ ...mockStrain });
   useStylesScoped$(StrainStyles);
 
   return (
     <>
       <div class="flex py-4">
-        {
-          <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4 drop-shadow">
-            <div class="flex flex-1 flex-row gap-2">
-              <div>
-                <img src={strain.img_url} alt="" width={200} height={200} />
-              </div>
-              <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4  ">
-                <span class="text-2xl font-medium">{strain.name}</span>
-                <div class="flex flex-row items-baseline">
-                  <p>aka</p> 
-                  <ul class="flex flex-row items-baseline">
-                  {strain.aliases.map((alias) => (
-                    <li class="px-1">
-                      {alias}
-                    </li>
-                  ))}
-                  </ul>
-                </div>
-                <ul class="flex flex-row items-baseline">
-                  <li> 
-                    {strain.genetic}
-                  </li>
-                  <li class="px-1">
-                    THC {strain.thc.min}-{strain.thc.max}%
-                  </li>
-                  <li class="px-1">
-                    CBD {strain.cbd.min}-{strain.cbd.max}%
-                  </li>
-                </ul>
-                <div class="flex flex-row items-baseline">
-                  <span>Price: {strain.price.min}-{strain.price.max} {strain.price.currency}</span> 
-                </div>
-              </div>
-              <div class="flex flex-col items-baseline">
-                <span class="text-4xl">{strain.rating.value}</span>
-                <span>****</span>
-                <span>{strain.rating.count} ratings</span>
-                <span>{strain.rating.likes} likes</span>
-              </div>
-            </div>
-            
-            <div class="bg-elevated p-4">
-              {strain.description}
+        <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4 drop-shadow">
+          {/* Main row */}
+          <div class="flex flex-1 flex-row gap-2">
+            {/* Image container */}
+            <div>
+              <img src={strain.img_url} alt="" width={200} height={200} />
             </div>
 
-            <div class="bg-elevated p-4">
-              STRAIN HIGHLIGHTS
-              <div>
-                <ul>
-                <li>
-                    <div class="flex flex-row items-baseline">
-                      Feelings: 
-                      {strain.feelings.map((feeling) => (
-                        <div class="px-1">
-                          {feeling.name}
-                        </div>
-                      ))}
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex flex-row items-baseline">
-                      Negatives: 
-                      {strain.negatives.map((negative) => (
-                        <div class="px-1">
-                          {negative.name}
-                        </div>
-                      ))}
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex flex-row items-baseline">
-                      Helps with: 
-                      {strain.helps.map((help) => (
-                        <div class="px-1">
-                          {help.name}
-                        </div>
-                      ))}
-                    </div>
-                  </li>
-                  <li>
-                    <div class="flex flex-row items-baseline">
-                      Flavor: 
-                      {strain.flavors.map((flavor) => (
-                        <div class="px-1">
-                          {flavor.name}
-                        </div>
-                      ))}
-                    </div>
-                  </li>
-                </ul>
+            {/* KPIs container */}
+            <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4  ">
+              <h1 class="text-2xl font-medium">{strain.name}</h1>
+              <div class="flex flex-row">
+                {'aka '.concat(strain.aliases.map((alias) => alias).join(', '))}
+              </div>
+              <ul class="flex flex-row">
+                <li>{strain.genetic}</li>
+                <li class="px-1">
+                  THC {strain.thc.min}-{strain.thc.max}%
+                </li>
+                <li class="px-1">
+                  CBD {strain.cbd.min}-{strain.cbd.max}%
+                </li>
+              </ul>
+              <div class="flex flex-row">
+                <span>
+                  Price: {strain.price.min}-{strain.price.max}{' '}
+                  {strain.price.currency}
+                </span>
               </div>
             </div>
+
+            {/* Ratings container */}
+            <div class="flex flex-col">
+              <span class="text-4xl">{strain.rating.value}</span>
+              <span>****</span>
+              <span>{strain.rating.count} ratings</span>
+              <span>{strain.rating.likes} likes</span>
+            </div>
           </div>
-        }
+
+          {/* Description row */}
+          <div class="p-4">{strain.description}</div>
+
+          {/* Highlights row */}
+          <div class="p-4">
+            <div>STRAIN HIGHLIGHTS</div>
+
+            {/* Feelings */}
+            <div class="flex flex-row gap-3">
+              <div>Feelings:</div>
+              {strain.feelings.map((feeling) => (
+                <Link href={`/strains?feeling=${feeling.name}`}>
+                  {feeling.name}
+                </Link>
+              ))}
+            </div>
+
+            {/* Negatives */}
+            <div class="flex flex-row gap-3">
+              <div>Negatives:</div>
+              {strain.negatives.map((negative) => (
+                <Link href={`/strains?negative=${negative.name}`}>
+                  {negative.name}
+                </Link>
+              ))}
+            </div>
+
+            {/* Helps */}
+            <div class="flex flex-row gap-3">
+              <div>Helps with:</div>
+              {strain.helps.map((help) => (
+                <Link href={`/strains?helps-with=${help.name}`}>
+                  {help.name}
+                </Link>
+              ))}
+            </div>
+
+            {/* Flavor */}
+            <div class="flex flex-row gap-3">
+              <div>Flavor:</div>
+              {strain.flavors.map((flavor) => (
+                <Link href={`/strains?flavor=${flavor.name}`}>
+                  {flavor.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
