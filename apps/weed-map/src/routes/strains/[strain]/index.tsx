fa85cf7e58eafb276a082185f05d57aa27f0f976
pazzicz <pazzicz@gmail.com>
2023-07-27 20:36:54 +0200

    strain - layout, replace stars with images

--- apps/weed-map/src/routes/strains/[strain]/index.tsx
+++ apps/weed-map/src/routes/strains/[strain]/index.tsx
@@ -1,13 +1,9 @@
-import {
-  component$,
-  useStore,
-  useStyles$,
-  useStylesScoped$,
-} from '@builder.io/qwik';
+import { component$, useStore, useStyles$, useStylesScoped$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { Link, routeLoader$ } from '@builder.io/qwik-city';
 import { getSupabaseClient } from '@api/client';
 import StrainStyles from './strain.css?inline';
+import { Icon } from '@ui/icon';
 
 export const head: DocumentHead = {
   title: 'Weed Map - Strain Detail',
@@ -67,11 +63,7 @@ const mockStrain = {
 export const useStrain = routeLoader$(async (req) => {
   const { strain } = req.params;
   const client = getSupabaseClient(req);
-  const { data } = await client
-    .from('strains')
-    .select('*')
-    .eq('slug', strain)
-    .single();
+  const { data } = await client.from('strains').select('*').eq('slug', strain).single();
   return data;
 });
 
@@ -84,41 +76,45 @@ export default component$(() => {
       <div class="flex py-4">
         <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4 drop-shadow">
           {/* Main row */}
-          <div class="flex flex-1 flex-row gap-2">
+          <div class="flex flex-wrap flex-1 flex-row gap-2">
             {/* Image container */}
-            <div>
-              <img src={strain.img_url} alt="" width={200} height={200} />
+            <div class="flex">
+              <img class="object-cover object-center h-96 w-96 rounded-xl" src={strain.img_url} />
             </div>
 
             {/* KPIs container */}
             <div class="flex flex-1 flex-col gap-2 bg-elevated rounded-md p-4  ">
-              <h1 class="text-2xl font-medium">{strain.name}</h1>
+              <div class="text-2xl font-medium">{strain.name}</div>
+              <div class="flex flex-row">{'aka '.concat(strain.aliases.map((alias) => alias).join(', '))}</div>
+
               <div class="flex flex-row">
-                {'aka '.concat(strain.aliases.map((alias) => alias).join(', '))}
-              </div>
-              <ul class="flex flex-row">
-                <li>{strain.genetic}</li>
-                <li class="px-1">
+                <div>{strain.genetic}</div>
+                <div class="px-1">
                   THC {strain.thc.min}-{strain.thc.max}%
-                </li>
-                <li class="px-1">
+                </div>
+                <div class="px-1">
                   CBD {strain.cbd.min}-{strain.cbd.max}%
-                </li>
-              </ul>
+                </div>
+              </div>
               <div class="flex flex-row">
                 <span>
-                  Price: {strain.price.min}-{strain.price.max}{' '}
-                  {strain.price.currency}
+                  Price: {strain.price.min}-{strain.price.max} {strain.price.currency}
                 </span>
               </div>
             </div>
 
             {/* Ratings container */}
-            <div class="flex flex-col">
-              <span class="text-4xl">{strain.rating.value}</span>
-              <span>****</span>
-              <span>{strain.rating.count} ratings</span>
-              <span>{strain.rating.likes} likes</span>
+            <div class="flex flex-col p-4 min-w-[10rem]">
+              <div class="text-4xl">{strain.rating.value}</div>
+              <div>
+                <Icon icon="star_rate_half" class="text-s"></Icon>
+                <Icon icon="star_rate" class="text-s"></Icon>
+                <Icon icon="star_rate" class="text-s"></Icon>
+                <Icon icon="star_rate" class="text-s"></Icon>
+                <Icon icon="star_rate" class="text-s"></Icon>
+              </div>
+              <div>{strain.rating.count} ratings</div>
+              <div>{strain.rating.likes} likes</div>
             </div>
           </div>
 
@@ -131,9 +127,12 @@ export default component$(() => {
 
             {/* Feelings */}
             <div class="flex flex-row gap-3">
-              <div>Feelings:</div>
+              <div class="flex flex-row">
+                <Icon icon="spa" class="text-base" />
+                <div>Feelings:</div>
+              </div>
               {strain.feelings.map((feeling) => (
-                <Link href={`/strains?feeling=${feeling.name}`}>
+                <Link href={`/strains?feeling=${feeling.name}`} class="hover:text-primary-600">
                   {feeling.name}
                 </Link>
               ))}
@@ -143,7 +142,7 @@ export default component$(() => {
             <div class="flex flex-row gap-3">
               <div>Negatives:</div>
               {strain.negatives.map((negative) => (
-                <Link href={`/strains?negative=${negative.name}`}>
+                <Link href={`/strains?negative=${negative.name}`} class="hover:text-primary-600">
                   {negative.name}
                 </Link>
               ))}
@@ -153,7 +152,7 @@ export default component$(() => {
             <div class="flex flex-row gap-3">
               <div>Helps with:</div>
               {strain.helps.map((help) => (
-                <Link href={`/strains?helps-with=${help.name}`}>
+                <Link href={`/strains?helps-with=${help.name}`} class="hover:text-primary-600">
                   {help.name}
                 </Link>
               ))}
@@ -163,7 +162,7 @@ export default component$(() => {
             <div class="flex flex-row gap-3">
               <div>Flavor:</div>
               {strain.flavors.map((flavor) => (
-                <Link href={`/strains?flavor=${flavor.name}`}>
+                <Link href={`/strains?flavor=${flavor.name}`} class="hover:text-primary-600">
                   {flavor.name}
                 </Link>
               ))}
